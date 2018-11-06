bf61b37972bb1be8de4d96e5a4561a83a2e1d9ea
Armando Meza Leyva <armezaleyva@gmail.com>
2018-11-06 08:33:28 -0700

    Clase 2018/11/06

--- PracticaMovimiento/MainWindow.xaml.cs
+++ PracticaMovimiento/MainWindow.xaml.cs
@@ -12,21 +12,64 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
+using System.Diagnostics;
 
 namespace PracticaMovimiento {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        Stopwatch stopwatch;
+        TimeSpan tiempoAnterior;
+
         public MainWindow() {
             InitializeComponent();
             miCanvas.Focus();
+
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+            tiempoAnterior = stopwatch.Elapsed;
+
+            ThreadStart threadStart = new ThreadStart(moverEnemigos);
+            Thread threadMoverEnemigos = new Thread(threadStart);
+            threadMoverEnemigos.Start();
+        }
+
+        void moverEnemigos() {
+            while(true) {
+                Dispatcher.Invoke(
+                () => {
+                    var tiempoActual = stopwatch.Elapsed;
+                    var deltaTime = tiempoActual - tiempoAnterior;
+
+                    double leftCarroActual = Canvas.GetLeft(imgCarro);
+                    Canvas.SetLeft(imgCarro, leftCarroActual + 90 * deltaTime.TotalSeconds);
+                    if(Canvas.GetLeft(imgCarro) >= 850) {
+                        Canvas.SetLeft(imgCarro, -100);
+                    }
+                    tiempoAnterior = tiempoActual;
+                }
+                );
+            }
         }
 
         private void miCanvas_KeyDown(object sender, KeyEventArgs e) {
             if (e.Key == Key.Up || e.Key == Key.W) {
                 double topGatoActual = Canvas.GetTop(imgGato);
                 Canvas.SetTop(imgGato, topGatoActual - 15);
+            }
+            if (e.Key == Key.Down || e.Key == Key.S) {
+                double topGatoActual = Canvas.GetTop(imgGato);
+                Canvas.SetTop(imgGato, topGatoActual + 15);
+            }
+            if (e.Key == Key.Left || e.Key == Key.A) {
+                double leftGatoActual = Canvas.GetLeft(imgGato);
+                Canvas.SetLeft(imgGato, leftGatoActual - 15);
+            }
+            if (e.Key == Key.Right || e.Key == Key.D) {
+                double leftGatoActual = Canvas.GetLeft(imgGato);
+                Canvas.SetLeft(imgGato, leftGatoActual + 15);
             }
         }
     }
